c5c86974a33c0daccc562f6cc3a21681c69d6304
jayesh-gupta-15 <jayeshsgupta1527@gmail.com>
2026-06-05 16:07:10 +0530

    c++ file created

---
+++ tp.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<string.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+
+    while(t--){
+        cout<<"Hello"<<endl;
+    }
+}
